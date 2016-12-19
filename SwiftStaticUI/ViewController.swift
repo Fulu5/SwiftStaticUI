8ba0a4217d8b03d0566d41a593419c4167206bb2
gxcao <1120643432@qq.com>
2016-12-19 19:26:20 +0800

    Init Project

---
+++ SwiftStaticUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStaticUI
+//
+//  Created by Guanxiong Cao  on 19/12/2016.
+//  Copyright © 2016 Guanxiong Cao . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
