031b67afce1b5d36b8b14032b705ea4e371e0d62
gxcao <1120643432@qq.com>
2016-12-19 19:42:31 +0800

    Add BackgroundView MainScrollView

--- SwiftStaticUI/ViewController.swift
+++ SwiftStaticUI/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mainScrollView: UIScrollView!
+    var headeImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configMainScrollView()
+        
+    }
+    
+    func configMainScrollView() {
+        mainScrollView = UIScrollView()
+        mainScrollView.backgroundColor = UIColor.gray
+        mainScrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainScrollView)
+        
+        mainScrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        mainScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mainScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        mainScrollView.heightAnchor.constraint(equalToConstant: 400).isActive = true
         
     }
 
