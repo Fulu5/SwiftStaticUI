0ffd089097fa990ec17f000dcb4d67e63de0d134
gxcao <1120643432@qq.com>
2016-12-25 22:20:35 +0800

    Take out infoView

--- SwiftStaticUI/ViewController.swift
+++ SwiftStaticUI/ViewController.swift
@@ -10,140 +10,120 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    let mainScrollView = UIScrollView()
-    
-    let descriptionView = UIView()
-    let titleLabel = UILabel()
-    let addressLabel = UILabel()
-    let availabelLabel = UILabel()
-    let viewButton = UIButton(type: UIButtonType.system)
-    let leftStackView = UIStackView()
-    let rightStackView = UIStackView()
-    let viewModel = ViewModel()
-    var imageSize = CGSize()
-    
-    
+    var mainScrollView: UIScrollView!
     var headImageView: HeadView!
-    
-    // sizeClass相关
-    var currentDeviceOrientation: UIDeviceOrientation = .unknown
-    fileprivate var regularConstraints = [NSLayoutConstraint]()
-    fileprivate var compactConstraints = [NSLayoutConstraint]()
-    
+    var infoView: InfoView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        configMainScrollView()
-    }
-    
-    func configMainScrollView() {
         
+        let viewModel = ViewModel()
         
+        mainScrollView = UIScrollView()
         view.addSubview(mainScrollView)
         
-        
-        
         headImageView = HeadView(image: UIImage(named: viewModel.imageName))
-        
-        
+        infoView = InfoView()
+        infoView.setValueForSubViews(viewModel: viewModel)
         
         mainScrollView.addSubview(headImageView)
-        
+        mainScrollView.addSubview(infoView)
         
         
         makeConstraints()
-        
-        
-        
+    }
+    
+//    func configMainScrollView() {
+    
         // descripView
-        descriptionView.translatesAutoresizingMaskIntoConstraints = false
-        mainScrollView.addSubview(descriptionView)
-        descriptionView.topAnchor.constraint(equalTo: headImageView.bottomAnchor, constant: 20).isActive = true
-        descriptionView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor, constant: 20).isActive = true
-        
-        let compactDescripTrail = descriptionView.trailingAnchor.constraint(equalTo: viewButton.leadingAnchor, constant: -20)
-        let regularDesCripTrail = descriptionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        
-        // label
-        addressLabel.text = viewModel.address
-        addressLabel.numberOfLines = 0
-        
-        addressLabel.font = UIFont.systemFont(ofSize: 12)
-        addressLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionView.addSubview(addressLabel)
-        
-        addressLabel.topAnchor.constraint(equalTo: descriptionView.topAnchor).isActive = true
-        addressLabel.leadingAnchor.constraint(equalTo: descriptionView.leadingAnchor).isActive = true
-        addressLabel.trailingAnchor.constraint(equalTo: descriptionView.trailingAnchor).isActive = true
-        addressLabel.bottomAnchor.constraint(equalTo: descriptionView.bottomAnchor).isActive = true
-        
-        // button
-        viewButton.setTitle(viewModel.buttonTitle, for: UIControlState.normal)
-        viewButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
-        viewButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
-        viewButton.layer.borderColor = UIColor.blue.cgColor
-        viewButton.layer.borderWidth = 1.0
-        viewButton.layer.cornerRadius = 2.0
-        viewButton.translatesAutoresizingMaskIntoConstraints = false
-        mainScrollView.addSubview(viewButton)
-
-        viewButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        viewButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        let viewButtonLeading = viewButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
-        let viewButtonTop = viewButton.topAnchor.constraint(equalTo: descriptionView.bottomAnchor, constant: 20)
-        let viewButtonTrail = viewButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        let viewButtonBottom = viewButton.bottomAnchor.constraint(equalTo: descriptionView.bottomAnchor)
-        
-        if viewModel.stackViewContent.count > 0 {
-            addStackView()
-        } else {
-            viewButton.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -20).isActive = true
-        }
-
-        // 根据不同的屏幕添加两套约束方案到约束数组中
-        compactConstraints.append(viewButtonBottom)
-        compactConstraints.append(viewButtonTrail)
-        compactConstraints.append(compactDescripTrail)
-        
-        regularConstraints.append(viewButtonTop)
-        regularConstraints.append(viewButtonLeading)
-        regularConstraints.append(regularDesCripTrail)
-        }
+//        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+//        mainScrollView.addSubview(descriptionView)
+//        descriptionView.topAnchor.constraint(equalTo: headImageView.bottomAnchor, constant: 20).isActive = true
+//        descriptionView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor, constant: 20).isActive = true
+//        
+//        let compactDescripTrail = descriptionView.trailingAnchor.constraint(equalTo: viewButton.leadingAnchor, constant: -20)
+//        let regularDesCripTrail = descriptionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+//        
+//        // label
+//        addressLabel.text = viewModel.address
+//        addressLabel.numberOfLines = 0
+//        
+//        addressLabel.font = UIFont.systemFont(ofSize: 12)
+//        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+//        descriptionView.addSubview(addressLabel)
+//        
+//        addressLabel.topAnchor.constraint(equalTo: descriptionView.topAnchor).isActive = true
+//        addressLabel.leadingAnchor.constraint(equalTo: descriptionView.leadingAnchor).isActive = true
+//        addressLabel.trailingAnchor.constraint(equalTo: descriptionView.trailingAnchor).isActive = true
+//        addressLabel.bottomAnchor.constraint(equalTo: descriptionView.bottomAnchor).isActive = true
+//        
+//        // button
+//        viewButton.setTitle(viewModel.buttonTitle, for: UIControlState.normal)
+//        viewButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+//        viewButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
+//        viewButton.layer.borderColor = UIColor.blue.cgColor
+//        viewButton.layer.borderWidth = 1.0
+//        viewButton.layer.cornerRadius = 2.0
+//        viewButton.translatesAutoresizingMaskIntoConstraints = false
+//        mainScrollView.addSubview(viewButton)
+//
+//        viewButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+//        viewButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
+//        let viewButtonLeading = viewButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+//        let viewButtonTop = viewButton.topAnchor.constraint(equalTo: descriptionView.bottomAnchor, constant: 20)
+//        let viewButtonTrail = viewButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+//        let viewButtonBottom = viewButton.bottomAnchor.constraint(equalTo: descriptionView.bottomAnchor)
+//        
+//        if viewModel.stackViewContent.count > 0 {
+//            addStackView()
+//        } else {
+//            viewButton.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -20).isActive = true
+//        }
+//
+//        // 根据不同的屏幕添加两套约束方案到约束数组中
+//        compactConstraints.append(viewButtonBottom)
+//        compactConstraints.append(viewButtonTrail)
+//        compactConstraints.append(compactDescripTrail)
+//        
+//        regularConstraints.append(viewButtonTop)
+//        regularConstraints.append(viewButtonLeading)
+//        regularConstraints.append(regularDesCripTrail)
+//        }
     
     // 添加stackView
-    func addStackView() {
-
-        leftStackView.translatesAutoresizingMaskIntoConstraints = false
-        rightStackView.translatesAutoresizingMaskIntoConstraints = false
-        leftStackView.axis = .vertical
-        rightStackView.axis = .vertical
-        leftStackView.alignment = .leading
-        rightStackView.alignment = .leading
-        leftStackView.distribution = .equalSpacing
-        rightStackView.distribution = .equalSpacing
-        mainScrollView.addSubview(leftStackView)
-        mainScrollView.addSubview(rightStackView)
-        
-        leftStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-        leftStackView.topAnchor.constraint(equalTo: viewButton.bottomAnchor, constant: 20).isActive = true
-        leftStackView.trailingAnchor.constraint(equalTo: rightStackView.leadingAnchor).isActive = true
-        leftStackView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -20).isActive = true
-        leftStackView.widthAnchor.constraint(equalTo: rightStackView.widthAnchor).isActive = true
-        rightStackView.leadingAnchor.constraint(equalTo: leftStackView.trailingAnchor).isActive = true
-        rightStackView.topAnchor.constraint(equalTo: leftStackView.topAnchor).isActive = true
-        rightStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        rightStackView.bottomAnchor.constraint(equalTo: leftStackView.bottomAnchor).isActive = true
-
-        for (key, value) in viewModel.stackViewContent {
-            let leftLabel = UILabel()
-            leftLabel.text = key
-            let rigthLabel = UILabel()
-            rigthLabel.text = value
-            leftStackView.addArrangedSubview(leftLabel)
-            rightStackView.addArrangedSubview(rigthLabel)
-        }
-        
-    }
+//    func addStackView() {
+//
+//        leftStackView.translatesAutoresizingMaskIntoConstraints = false
+//        rightStackView.translatesAutoresizingMaskIntoConstraints = false
+//        leftStackView.axis = .vertical
+//        rightStackView.axis = .vertical
+//        leftStackView.alignment = .leading
+//        rightStackView.alignment = .leading
+//        leftStackView.distribution = .equalSpacing
+//        rightStackView.distribution = .equalSpacing
+//        mainScrollView.addSubview(leftStackView)
+//        mainScrollView.addSubview(rightStackView)
+//        
+//        leftStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+//        leftStackView.topAnchor.constraint(equalTo: viewButton.bottomAnchor, constant: 20).isActive = true
+//        leftStackView.trailingAnchor.constraint(equalTo: rightStackView.leadingAnchor).isActive = true
+//        leftStackView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor, constant: -20).isActive = true
+//        leftStackView.widthAnchor.constraint(equalTo: rightStackView.widthAnchor).isActive = true
+//        rightStackView.leadingAnchor.constraint(equalTo: leftStackView.trailingAnchor).isActive = true
+//        rightStackView.topAnchor.constraint(equalTo: leftStackView.topAnchor).isActive = true
+//        rightStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+//        rightStackView.bottomAnchor.constraint(equalTo: leftStackView.bottomAnchor).isActive = true
+//
+//        for (key, value) in viewModel.stackViewContent {
+//            let leftLabel = UILabel()
+//            leftLabel.text = key
+//            let rigthLabel = UILabel()
+//            rigthLabel.text = value
+//            leftStackView.addArrangedSubview(leftLabel)
+//            rightStackView.addArrangedSubview(rigthLabel)
+//        }
+//        
+//    }
     
     
     private func makeConstraints() {
@@ -161,33 +141,20 @@ class ViewController: UIViewController {
         headImageView.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor).isActive = true
         headImageView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor).isActive = true
         headImageView.heightAnchor.constraint(equalTo: mainScrollView.widthAnchor, multiplier: headImageView.ratio).isActive = true
-    }
-    
-    // 监测屏幕旋转的方法
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
+        // infoView
+        infoView.translatesAutoresizingMaskIntoConstraints = false
+        infoView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor, constant: 20).isActive = true
+        infoView.trailingAnchor.constraint(equalTo: mainScrollView.trailingAnchor, constant: -20).isActive = true
+        infoView.topAnchor.constraint(equalTo: headImageView.bottomAnchor, constant: 20).isActive = true
         
         
-        // 1.regular情形
-        if traitCollection.horizontalSizeClass == .regular {
-            // 使compact下的约束失效
-            NSLayoutConstraint.deactivate(compactConstraints)
-            // 使regular下的约束生效
-            NSLayoutConstraint.activate(regularConstraints)
-        } else {
-            // 2.compact情形
-            NSLayoutConstraint.deactivate(regularConstraints)
-            NSLayoutConstraint.activate(compactConstraints)
-        }
+        
+        // 最底部的元素需要设定其与mainScrollView底部的约束
+        // 以此来撑大scrollView的contentSize
+        infoView.bottomAnchor.constraint(lessThanOrEqualTo: mainScrollView.bottomAnchor).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return UIStatusBarStyle.lightContent
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
--- SwiftStaticUI/Views/InfoView.swift
+++ SwiftStaticUI/Views/InfoView.swift
@@ -9,13 +9,97 @@
 import UIKit
 
 class InfoView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var nameLabel: UILabel!
+    var addressLabel: UILabel!
+    var viewButton: UIButton!
+    
+    var compactConstraints = [NSLayoutConstraint]()
+    var regularConstraints = [NSLayoutConstraint]()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        nameLabel = createNameLabel()
+        addressLabel = createAddressLabel()
+        viewButton = createDetailButton()
+        
+        addSubview(nameLabel)
+        addSubview(addressLabel)
+        addSubview(viewButton)
+        
+        setUpConstraints()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setValueForSubViews(viewModel: ViewModel) {
+        nameLabel.text = viewModel.name
+        addressLabel.text = viewModel.address
+        viewButton.setTitle(viewModel.buttonTitle, for: .normal)
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if traitCollection.horizontalSizeClass == .compact {
+            NSLayoutConstraint.deactivate(regularConstraints)
+            NSLayoutConstraint.activate(compactConstraints)
+        } else {
+            NSLayoutConstraint.deactivate(compactConstraints)
+            NSLayoutConstraint.activate(regularConstraints)
+        }
+    }
+    
+    func setUpConstraints() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        addressLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        
+        viewButton.translatesAutoresizingMaskIntoConstraints = false
+        viewButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        compactConstraints.append(nameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor))
+        compactConstraints.append(addressLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor))
+        compactConstraints.append(viewButton.leadingAnchor.constraint(equalTo: self.leadingAnchor))
+        compactConstraints.append(viewButton.leadingAnchor.constraint(equalTo: self.leadingAnchor))
+        compactConstraints.append(viewButton.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 20))
+        compactConstraints.append(viewButton.bottomAnchor.constraint(equalTo: self.bottomAnchor))
+        
+        regularConstraints.append(nameLabel.trailingAnchor.constraint(equalTo: viewButton.leadingAnchor))
+        regularConstraints.append(addressLabel.widthAnchor.constraint(equalTo: nameLabel.widthAnchor))
+        regularConstraints.append(addressLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor))
+        regularConstraints.append(viewButton.topAnchor.constraint(equalTo: self.topAnchor))
+        regularConstraints.append(viewButton.widthAnchor.constraint(equalToConstant: 150))
+    }
+    
+    func createNameLabel() -> UILabel {
+        let nameLabel = UILabel()
+        nameLabel.font = UIFont.systemFont(ofSize: 20)
+        return nameLabel
+    }
+    
+    func createAddressLabel() -> UILabel {
+        let addressLabel = UILabel()
+        addressLabel.font = UIFont.systemFont(ofSize: 16)
+        addressLabel.numberOfLines = 0
+        return addressLabel
+    }
+    
+    func createDetailButton() -> UIButton {
+        let detailButton = UIButton()
+        detailButton.setTitleColor(UIColor.blue, for: .normal)
+        detailButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        detailButton.layer.borderWidth = 1.0
+        detailButton.layer.borderColor = UIColor.blue.cgColor
+        detailButton.layer.cornerRadius = 3.0
+        detailButton.contentEdgeInsets = UIEdgeInsetsMake(12.0, 20.0, 12.0, 20.0)
+        return detailButton
+    }
 }
